b7f3b9390046282bb41762ee42248b9fc9ffe7c8
mauroacampani <mauroacampani@gmail.com>
2023-05-28 21:18:41 -0300

    Se agrego archivo contenido.js en el cual se crea el html del header y del footer

---
+++ js/contenido.js
@@ -0,0 +1,26 @@
+
+	let	enc = `
+		 <div class="contenedor">
+        <nav id="navbar">
+            
+            <ul class="menu">
+                <li class="logo"><a href="#"><img src="img/logo.jpg" alt="" ></a></li>
+                <li class="item"><a  href="index.html">Inicio</a></li>
+                <li class="item"><a  href="peliculas.html">Películas</a></li>
+                <li class="item"><a  href="series.html">Series</a></li>
+                <li class="item"><a  href="contacto.html">Contacto</a></li>
+                <li class="toggle"><a id="btn-a" href="#"><i class="fas fa-bars"></i></a></li>
+            </ul>
+        </nav>
+    </div>
+	`
+	document.querySelector("header").innerHTML = enc;
+
+
+let pie = `
+<div style="text-align: center;">Disenado por Equipo 8</div>
+                <a href="https://www.instagram.com/"><img id=redes src="img/instagram.png" alt="Instagram" width="25px"></a>
+                <a href="https://es-la.facebook.com/"><img id=redes src="img/facebook.png" alt="Facebook" width="25px"></a>
+                <a href="https://www.whatsapp.com/?lang=es"><img id=redes src="img/whatsapp.png" alt="Whatsapp" width="25px"></a>
+                `
+ document.querySelector("footer").innerHTML = pie;              
--- js/main.js
+++ js/main.js
@@ -315,3 +315,5 @@ function detenerVideo() {
 
 
 
+
+
